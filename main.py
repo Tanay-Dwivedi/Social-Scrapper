b65fc930126492cd95c17f80fa67d314a9e1a2b8
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2024-01-18 22:47:43 +0530

    GitHub scraping done

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ social_media = sl.selectbox(
     "Social Media Platform", options=["Linkedin", "Instagram", "GitHub", "YouTube"]
 )
 
-with sl.form("Form 1", clear_on_submit=True):
+with sl.form("Form 1"):
     username = sl.text_input("Enter the username")
     submit_button = sl.form_submit_button("Scrape")
 
-sl.subheader("Scrapped Data")
+sl.write("## Scrapped Data")
 
 
 def get_data(username):
@@ -50,31 +50,48 @@ def link_converter(username):
 def github_data_onclick():
     scraped_data = get_data(username)
     if scraped_data[0] == "Data not available" or scraped_data == "":
-        sl.error("Account data not available")
+        sl.error("### Account data not available")
     else:
-        sl.write(f"Name: {scraped_data[0]}")
-        sl.image(scraped_data[1], width=300, caption="Profile Image")
-        sl.write(f"Bio: {scraped_data[2]}")
+        sl.write("##")
+        if scraped_data[0] == "":
+            sl.warning("Name not available")
+        else:
+            sl.write(f"### Name: {scraped_data[0]}")
+        sl.write("##")
+        sl.image(scraped_data[1], width=300, caption=f"{username}'s Image")
+        sl.write("##")
+        if scraped_data[2] == "":
+            sl.warning("Bio not available")
+        else:
+            sl.write("### Bio: ")
+            sl.write(f"{scraped_data[2]}")
+        sl.write("##")
         try:
-            sl.write(f"Followers: {scraped_data[3]}")
+            sl.write(f"### Followers: {scraped_data[3]}")
         except:
-            sl.warning("Followers not available")
+            sl.warning("### Followers not available")
         try:
-            sl.write(f"Following: {scraped_data[4]}")
+            sl.write(f"### Following: {scraped_data[4]}")
         except:
-            sl.warning("Followings not available")
+            sl.warning("### Followings not available")
+        sl.write("##")
+        sl.write(f"### {username}'s contribution badges: ")
         try:
             badges_row = sl.columns(len(scraped_data[5]))
             for index, link in enumerate(scraped_data[5]):
                 badges_row[index].image(link, width=100)
         except:
             sl.warning("Badges not available")
-        sl.write(f"Public Contributions: {scraped_data[6]}")
-        sl.write(f"Commit overview: {scraped_data[7][21:]}")
+        sl.write("##")
+        sl.write(f"### Total public commits: {scraped_data[6]}")
+        sl.write("##")
+        sl.write("### Commit overview: ")
+        sl.write(f"{scraped_data[7][21:]}")
+        sl.write("##")
         for repo in scraped_data[8]:
             heading = str(repo["serial_number"]) + "- " + repo["name"]
             with sl.expander(heading):
-                sl.write("URL: " + repo["url"])
+                sl.markdown("""Link - [Link to project](repo["url"])""")
                 if repo["description"] == "Data not available":
                     sl.warning("Description of the project is not available")
                 else:
